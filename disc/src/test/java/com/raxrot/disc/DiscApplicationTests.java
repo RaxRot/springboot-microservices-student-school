c357bd08eccfd20e8369142692f50158754cac65
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-01 20:39:09 +0100

    add discovery server

---
+++ disc/src/test/java/com/raxrot/disc/DiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.disc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
