120e5903ba9849b476a350b53f52565df6a7ac62
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-01 22:18:45 +0100

    create school service

---
+++ school/src/test/java/com/raxrot/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
